49046d3089d876d1b9e11cd98be2c324f4d67cb9
CasperL1218 <cl2622@cornell.edu>
2026-03-28 11:20:14 -0400

    movie-list-scraping

---
+++ movie-list-scrap.R
@@ -0,0 +1,31 @@
+library(rvest)
+library(purrr)
+library(dplyr)
+
+page <- read_html("Academyc Best Picture 2000 2025.html")
+year_groups <- page |> html_elements(".result-group")
+
+oscar_list <- map(year_groups, function(group) {
+  year        <- group |> html_element(".result-group-title a") |> html_text2()
+  nominations <- group |> html_elements(".result-details")
+  
+  films <- map_dfr(nominations, function(nom) {
+    film   <- nom |> html_element(".awards-result-film-title a") |> html_text2()
+    winner <- length(html_elements(nom, ".glyphicon-star")) > 0
+    tibble(film = film, winner = winner)
+  }) |> filter(!is.na(film))
+  
+  list(year = year, films = films)
+})
+
+names(oscar_list) <- map_chr(oscar_list, "year")
+
+df <- map_dfr(oscar_list, ~ .x$films |> mutate(year = .x$year)) |>
+  select(year, film, winner) |>
+  mutate(
+    ceremony = as.integer(stringr::str_extract(year, "(?<=\\()\\d+")),
+    year     = as.integer(stringr::str_extract(year, "\\d{4}"))
+  ) |>
+  select(year, ceremony, film, winner)
+
+
