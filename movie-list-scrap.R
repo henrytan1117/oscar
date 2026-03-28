e9b51f0438fa5b78c9367129e1d46f9166f868a0
henrytan <ht492@cornell.edu>
2026-03-28 11:38:48 -0400

    Add a list of movie in csv

--- movie-list-scrap.R
+++ movie-list-scrap.R
@@ -1,3 +1,7 @@
+### This file is used to extract the list of nominated films
+### From 2000 to 2024
+
+
 library(rvest)
 library(purrr)
 library(dplyr)
@@ -27,4 +31,41 @@ df <- map_dfr(oscar_list, ~ .x$films |> mutate(year = .x$year)) |>
     year     = as.integer(stringr::str_extract(year, "\\d{4}"))
   ) |>
   select(year, ceremony, film, winner)
+
+
+# Convert the file above into dataframe
+df <- data.frame(df)
+
+# The Academy Awards catalogue is not fully updated with 2025 data
+# So we will manually add the 2025 data into the data frame
+
+# Create a new dataframe for the 2025 nominees
+df_2025 <- data.frame(
+  year = 2025,
+  ceremony = 98,
+  film = c(
+    "One Battle After Another", 
+    "Bugonia", 
+    "F1", 
+    "Frankenstein", 
+    "Hamnet", 
+    "Marty Supreme", 
+    "The Secret Agent", 
+    "Sentimental Value", 
+    "Sinners", 
+    "Train Dreams"
+  ),
+  winner = c(TRUE, rep(FALSE, 9)), # One Battler After Another is the winner (TRUE), others are FALSE
+  stringsAsFactors = FALSE
+)
+
+# Combine with your existing dataframe
+# (Assuming your current dataframe is named 'df')
+df <- rbind(df, df_2025)
+
+# Verify the update
+tail(df, 10)
+
+
+
 
