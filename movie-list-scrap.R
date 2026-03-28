976d599fda7b829a4aa46e84187e78c5875f5be5
henrytan <ht492@cornell.edu>
2026-03-28 11:10:50 -0400

    Simply the html file name

---
+++ movie-list-scrap.R
@@ -0,0 +1,5 @@
+### This file is used to scrap the list of nominated movies for Best Picture Awards
+### From 2000 to 2025
+### From Academy Awards Public Database
+### The html file is downloaded from https://awardsdatabase.oscars.org/search/results
+
